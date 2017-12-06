e5fc08b5b4ba72c34be0bbe94369dddf3c77301d
JosephSpearritt <joseph.spearritt@uq.net.au>
2017-12-06 10:33:10 +1000

    Fix haskell day 5 riparino

--- aoc17/src/Day5.hs
+++ aoc17/src/Day5.hs
@@ -1,5 +1,6 @@
 module Day5 where
 
+import Data.List
 import Control.Monad.State
 
 import qualified Data.Vector as V
@@ -22,31 +23,42 @@ main = do
         Left _ -> putStrLn "Parsing failed"
         Right d -> do
             -- putStrLn $ show $ sum d
-            -- putStrLn $ "Part 1: It will take " ++ (show $ numStepsToEscape d (+1)) ++ " steps to escape"
-            putStrLn $ "Part 2: It will take " ++ (show $ numStepsToEscape d pt2UpdateFn) ++ " steps to escape"
+            putStrLn $ "Part 1: It will take " ++ (show $ numStepsToEscapeNoState d (+1)) ++ " steps to escape"
+            putStrLn $ "Part 2: It will take " ++ (show $ numStepsToEscapeNoState d pt2UpdateFn) ++ " steps to escape"
 
 -- Input parsing
 
 parserInput :: Parser [Integer]
 parserInput = int `sepBy` endOfLine
 
--- Part 1
+-- Implementation without using State
+
+numStepsToEscapeNoState :: [Integer] -> (Integer -> Integer) -> Integer
+numStepsToEscapeNoState prog updateFn = runProgram updateFn (Execution (V.fromList prog) 0)
+
+runProgram :: (Integer -> Integer) -> Execution -> Integer
+runProgram updateFn = toInteger . length . unfoldr (runStep updateFn)
+
+runStep :: (Integer -> Integer) -> Execution -> Maybe (Integer, Execution)
+runStep updateFn execution
+    | pos < 0 || pos > (toInteger (V.length instrs - 1)) = Nothing
+    | otherwise = Just $ (1, Execution newInstrs newPos)
+    where pos = programCounter execution
+          instrs = instructions execution
+          curInstr = toInteger $ instrs V.! (fromIntegral pos)
+          newPos = pos + curInstr
+          newInstrs = instrs V.// [(fromIntegral pos, updateFn curInstr)]
+
+pt2UpdateFn :: Integer -> Integer
+pt2UpdateFn val = if (val >= 3) then
+                    val - 1
+                else
+                    val + 1
+
+-- Old implementation that hangs lol
 
 numStepsToEscape :: [Integer] -> (Integer -> Integer) -> Integer
 numStepsToEscape prog updateFn = evalState (executeProgram 0 updateFn) (Execution (V.fromList prog) 0)
-
--- runProgram :: Integer -> Execution -> Integer
--- runProgram steps execution = if pos < 0 || pos >
---     where pos = programCounter execution
---           instrs = instructions execution 
-
--- runStep :: Execution -> Execution
--- runStep execution = Execution newInstrs newPos
---     where pos = programCounter execution
---           instrs = instructions execution
---           curInstr = toInteger $ instrs V.! (fromIntegral pos)
---           newPos = pos + curInstr
---           newInstrs = instrs V.// [(fromIntegral pos, curInstr + 1)]
 
 executeProgram :: Integer -> (Integer -> Integer) -> State Execution Integer
 executeProgram steps updateFn = do
@@ -59,14 +71,13 @@ executeProgram steps updateFn = do
     else
         executeProgram (steps + n) updateFn
 
-
 executeStep :: (Integer -> Integer) -> State Execution Integer
 executeStep updateFn = do
     execution <- get
     let instrs = instructions execution
     let pos = programCounter execution
     let curInstr = toInteger $ instrs V.! (fromIntegral pos)
-    if pos < 0 || pos > (toInteger $ V.length instrs - 1) then
+    if pos < 0 || pos > (toInteger (V.length instrs - 1)) then
         -- Program is done, don't take a step
         return 0
     else do
@@ -75,13 +86,3 @@ executeStep updateFn = do
         let newInstrs = instrs V.// [(fromIntegral pos, updateFn curInstr)]
         put $ Execution newInstrs newPos
         return 1
-
-
-
--- Part 2
-
-pt2UpdateFn :: Integer -> Integer
-pt2UpdateFn val = if (val >= 3) then
-                      val - 1
-                  else
-                      val + 1
